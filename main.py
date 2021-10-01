ca5ecb9eec51c9615c8c7d2d00165536cb5ef706
Clepalitto <85410090+Clepalitto@users.noreply.github.com>
2021-10-01 16:06:22 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,45 @@
+import wikipediaapi
+import datetime
+
+day_now = datetime.datetime.now().day
+month_now = datetime.datetime.now().month
+
+if month_now == 1:
+    month = 'January'
+if month_now == 2:
+    month = 'February'
+if month_now == 3:
+    month = 'March'
+if month_now == 4:
+    month = 'April'
+if month_now == 5:
+    month = 'May'
+if month_now == 6:
+    month = 'June'
+if month_now == 7:
+    month = 'July'
+if month_now == 8:
+    month = 'August'
+if month_now == 9:
+    month = 'September'
+if month_now == 10:
+    month = 'October'
+if month_now == 11:
+    month = 'November'
+if month_now == 12:
+    month = 'December'
+
+
+day_now = str(day_now)
+
+title_page = str(month+'_'+day_now)
+
+wikipedia = wikipediaapi.Wikipedia('en')
+page = wikipedia.page(title_page)
+print("")
+print("Wooh - What's up today ?????")
+print("")
+print(page.summary[0:60])
+print("")
+h = page.section_by_title('Holidays and observances')
+print(h)
